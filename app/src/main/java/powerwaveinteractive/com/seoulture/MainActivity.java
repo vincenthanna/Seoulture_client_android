3a1815291b54fb59bedbe23dedb2e88567ea5b34
vincenthanna <firsttimelove@gmail.com>
2014-08-20 14:56:37 +0900

    Initial Commit

---
+++ app/src/main/java/powerwaveinteractive/com/seoulture/MainActivity.java
@@ -0,0 +1,71 @@
+package powerwaveinteractive.com.seoulture;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import SlidingTabs.SlidingTabLayout;
+import powerwaveinteractive.com.tabsswipe.adapter.TabsPagerAdapter;
+
+
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener{
+
+    private SlidingTabLayout mSlidingTabLayout;
+    private ViewPager viewPager;
+    private TabsPagerAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initilization
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(mAdapter);
+
+        // SlidingTabLayout 설정
+        mSlidingTabLayout = (SlidingTabLayout)findViewById(R.id.sliding_tabs);
+        mSlidingTabLayout.setViewPager(viewPager);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+}
---
+++ app/src/main/java/powerwaveinteractive/com/tabsswipe/adapter/TabsPagerAdapter.java
@@ -0,0 +1,45 @@
+package powerwaveinteractive.com.tabsswipe.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import powerwaveinteractive.com.seoulture.*;
+
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+    public TabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    private String[] tabs = { "Top Rated Contents", "Games List", "Hot Movies & TV Program" };
+
+    @Override
+    public Fragment getItem(int index) {
+
+        switch (index) {
+            case 0:
+                // Top Rated fragment activity
+                return new TopRatedFragment();
+            case 1:
+                // Games fragment activity
+                return new GamesFragment();
+            case 2:
+                // Movies fragment activity
+                return new MoviesFragment();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // get item count - equal to number of tabs
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs[position];
+    }
+}
